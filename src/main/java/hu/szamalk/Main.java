909f7cb7ddf0c2c157d50f935ddd9fc314121ea7
WeimperGergo <weimper.geri@gmail.com>
2024-12-09 15:10:00 +0100

    f2 kezdes

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -3,9 +3,24 @@ package hu.szamalk;
 
 import hu.szamalk.modell.Tabla;
 
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-
+        Tabla T1 = new Tabla('#');
+        // f4
+        System.out.println("4. feladat: Az üres tábla: ");
+        T1.megjelenit();
+        // f5-6
+        System.out.println("6. feladat: A feltöltött tábla: ");
+        T1.elhelyez(8);
+        T1.megjelenit();
+        // f7
+        //System.out.println(T1.uresSor(2));
+        //System.out.println(T1.uresOszlop(7));
+        // f8-9
+        System.out.println("\n9. feladat: \nOszlopok: " + T1.uresOszlopSzama());
+        System.out.println("Sorok: " + T1.uresSorSzama());
 
 
     }
--- src/main/java/hu/szamalk/modell/Tabla.java
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -1,18 +1,93 @@
 package hu.szamalk.modell;
 
+import java.util.Random;
+
 public class Tabla {
-    private Character[][] T;
-    private Character UresCella;
+    private Character[][] t;
+    private Character uresCella;
+    private static Random rnd = new Random();
 
     public Tabla(Character uresCella) {
-        T = new Character[8][8];
-        UresCella = uresCella;
-        for (int i = 0; i < T.length; i++) {
-            for (int j = 0; j < T[i].length; j++) {
-                T[i][j] = UresCella;
+        t = new Character[8][8];
+        this.uresCella = uresCella;
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < t[i].length; j++) {
+                t[i][j] = this.uresCella;
             }
         }
-
-
     }
+
+    public void megjelenit(){
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < t[i].length; j++) {
+                System.out.print(t[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void elhelyez(int n){
+        int i = 0;
+        while (i < n) {
+            int x, y;
+            x = rnd.nextInt(0, 8);
+            y = rnd.nextInt(0, 8);
+            if (t[x][y] == uresCella){
+                t[x][y] = 'K';
+                i++;
+            }
+        }
+    }
+
+    public boolean uresOszlop(int y){
+        int i = 0;
+        boolean igaz = true;
+        while (i < t.length) {
+            if (t[i][y] == uresCella){
+                i++;
+            }
+            else {
+                igaz = false;
+                i = t.length;
+            }
+        }
+        return igaz;
+    }
+
+    public boolean uresSor(int x){
+        int i = 0;
+        boolean igaz = true;
+        while (i < t[x].length) {
+            if (t[x][i] == uresCella){
+                i++;
+            }
+            else {
+                igaz = false;
+                i = t[x].length;
+            }
+        }
+        return igaz;
+    }
+
+    public int uresSorSzama(){
+        int ossz = 0;
+        for (int i = 0; i < t.length; i++) {
+            if (uresSor(i)) ossz++;
+        }
+        return ossz;
+    }
+
+    public int uresOszlopSzama(){
+        int ossz = 0;
+        for (int i = 0; i < t.length; i++) {
+            if (uresOszlop(i)) ossz++;
+        }
+        return ossz;
+    }
+
+
+
+
+
 }
